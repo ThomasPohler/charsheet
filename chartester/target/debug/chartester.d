d294d7eb2c1228a570d1cf5e395b568ab5db864f
Thomas Pohler <tpp0012@auburn.edu>
2024-01-26 00:57:21 -0500

    backup: commit

--- chartester/target/debug/chartester.d
+++
@@ -1,1 +0,0 @@
-C:\Users\solar\Desktop\charsheet\chartester\target\debug\chartester.exe: C:\Users\solar\Desktop\charsheet\chartester\src\main.rs
--- chartester/target/debug/deps/chartester.d
+++
@@ -1,5 +0,0 @@
-C:\Users\solar\Desktop\charsheet\chartester\target\debug\deps\chartester.exe: src\main.rs
-
-C:\Users\solar\Desktop\charsheet\chartester\target\debug\deps\chartester.d: src\main.rs
-
-src\main.rs:
